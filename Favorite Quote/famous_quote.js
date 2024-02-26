9dc8ee0564fd5cd86f715cab5c5acc03b39d283f
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-02-26 17:30:49 +0500

    Add files via upload

---
+++ Favorite Quote/famous_quote.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log('Mohammad Ali once Said, "I hated every minute of training, but i use to say my self, Dont quit. Suffer now and live the rest of your life as a champion."');
