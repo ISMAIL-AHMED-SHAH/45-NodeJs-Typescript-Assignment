09757cd27ac7228f67e38d3463e21faed4dd9897
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-02-26 20:18:45 +0500

    Add files via upload

---
+++ 5.Famous Quote/Famous_Quote2.js
@@ -0,0 +1,4 @@
+"use strict";
+let Famous_person = 'H.Jackson Brown';
+let message = '"The best preparation for tomorrow is doing your best today."';
+console.log(`${Famous_person} once said, ${message}`);
---
+++ 5.Famous Quote/Famous_Quote2.ts
@@ -0,0 +1,5 @@
+let Famous_person :string = 'H.Jackson Brown';
+
+let message :string = '"The best preparation for tomorrow is doing your best today."';
+
+console.log(`${Famous_person} once said, ${message}`);
