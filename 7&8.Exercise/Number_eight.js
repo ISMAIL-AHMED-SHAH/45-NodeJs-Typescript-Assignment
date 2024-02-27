fe2a8f0330a2a7d455001d77dac32df57b66c76e
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-02-27 19:50:00 +0500

    Add files via upload

---
+++ 7&8.Exercise/Number_eight.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(2 + 6); // addition
+console.log(9 - 1); // subtraction
+console.log(2 * 4); // multiplication
+console.log(24 / 3); // division
---
+++ 7&8.Exercise/Number_eight.ts
@@ -0,0 +1,8 @@
+console.log(2+6); // addition
+
+console.log(9-1); // subtraction
+
+console.log(2*4); // multiplication
+
+console.log(24/3); // division
+
