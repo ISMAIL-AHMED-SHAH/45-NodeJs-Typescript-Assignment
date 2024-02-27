8d1d0418e83fdf2c1f9a85091fcc7133248e192c
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-02-27 19:38:57 +0500

    Add files via upload

---
+++ 6.Striping Names/Striping_names.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = '    \n\tIsmail Ahmed Shah\n\t    ';
+console.log(personName);
+const without_Whitespace = personName.trim();
+console.log(without_Whitespace);
---
+++ 6.Striping Names/Striping_names.ts
@@ -0,0 +1,6 @@
+const personName :string = '    \n\tIsmail Ahmed Shah\n\t    ';
+console.log(personName);
+
+const without_Whitespace :string = personName.trim();
+
+console.log(without_Whitespace);
