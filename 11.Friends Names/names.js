8e03cb9c91f035820a124f25e61fe7ac316e761e
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-01 22:22:58 +0500

    Add files via upload

---
+++ 11.Friends Names/names.js
@@ -0,0 +1,6 @@
+// let names = ['ahmed', 'ismail', 'shah','Syed];
+// names.forEach(names => console.log(names));
+var names = ['ismail', 'ahmed', 'shah', 'Syed'];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11.Friends Names/names.ts
@@ -0,0 +1,10 @@
+
+// let names = ['ahmed', 'ismail', 'shah','Syed];
+
+// names.forEach(names => console.log(names));
+
+let names :string[] = ['ismail', 'ahmed', 'shah', 'Syed'];
+
+for(let i=0; i<names.length; i++){
+    console.log(names[i]);
+}
