89158be3d755eced3ae06951a93d5ae296c43b49
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-01 22:37:49 +0500

    Add files via upload

---
+++ 12.Greetings/names.js
@@ -0,0 +1,7 @@
+// let names :string[] = ['ismail', 'ahmed', 'shah', 'Syed'];
+// const message :string = 'Welcome to Governor initiative IT program: '
+// for(let i=0; i<names.length; i++){
+//     console.log(message + names[i]);
+// }
+var names = ['ismail', 'ahmed', 'shah', 'Syed'];
+names.forEach(function (names) { return console.log("Hello ".concat(names, ", How are you doing today.")); });
