97098515149094c7ca4a07a1b0dca35a65a01cd5
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-01 21:48:16 +0500

    Add files via upload

---
+++ 9.Favorite Number/Favorite_number.js
@@ -0,0 +1,3 @@
+var favorite_Number = 4;
+var message = favorite_Number + ' is my favorite number';
+console.log(message);
---
+++ 9.Favorite Number/Favorite_number.ts
@@ -0,0 +1,5 @@
+const favorite_Number : number = 4;
+
+const message :string = favorite_Number + ' is my favorite number';
+
+console.log(message);
