24a0d8e2140efb0e06ef4c0b19c91a90c7277569
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-01 23:09:23 +0500

    Add files via upload

---
+++ 14.Guest List for Dinner/Guest_list.js
@@ -0,0 +1,7 @@
+"use strict";
+// let Guest_list = ['ismail', 'ahmed', 'shah','Syed'];
+// for (let i = 0; i < Guest_list.length; i++) {
+//     console.log('Dear Mr. ' + Guest_list[i] + ',\n\n I am hosting a party on coming weekend. I promise it will be a blast, and your presence would make it even more special. Looking forward to catching up and having a fantastic time together. \n\nThank you!\n\n');
+// }
+let Guest_list = ['ismail', 'ahmed', 'shah', 'Syed'];
+Guest_list.forEach(Guest_list => console.log(`Dear Mr. ${Guest_list} I am hosting a party on coming weekend. I promise it will be a blast, and your presence would make it even more special. Looking forward to catching up and having a fantastic time together.\n Warm Regards\n`));
