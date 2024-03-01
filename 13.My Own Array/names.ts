0d2ed95595c110286c6114128c9bd7142d0b79fb
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-01 22:50:09 +0500

    Add files via upload

---
+++ 13.My Own Array/names.ts
@@ -0,0 +1,9 @@
+// let names :string[] = ['ismail', 'ahmed', 'shah', 'Syed'];
+
+// const message :string = 'Welcome to Governor initiative IT program: '
+// for(let i=0; i<names.length; i++){
+//     console.log(message + names[i]);
+// }
+
+let names :string[] = ['ismail', 'ahmed', 'shah','Syed'];
+names.forEach(names => console.log(`Hello ${names}, How are you doing today.`))
