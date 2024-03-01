fbd28fa083c1fb2803a76172e6898e18ff969b51
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-01 23:21:58 +0500

    Add files via upload

---
+++ 13.My Own Array/My_Own_Array.js
@@ -0,0 +1,7 @@
+"use strict";
+let transpotation = ['motorcycle', 'sports car', 'cycle', 'bus'];
+for (let i = 0; i < transpotation.length; i++) {
+    console.log('i would like to own a ' + transpotation[i]);
+}
+// let transpotation :string[]= ['motorcycle','sports car','cycle','bus'] ;
+// transpotation.forEach(transpotation => console.log(`i would like to own a ${transpotation}`))
---
+++ 13.My Own Array/My_Own_Array.ts
@@ -0,0 +1,9 @@
+let transpotation :string[]= ['motorcycle','sports car','cycle','bus'] ;
+
+for (let i=0; i<transpotation.length; i++){
+    console.log('i would like to own a ' + transpotation[i] )
+}
+
+// let transpotation :string[]= ['motorcycle','sports car','cycle','bus'] ;
+
+// transpotation.forEach(transpotation => console.log(`i would like to own a ${transpotation}`))
