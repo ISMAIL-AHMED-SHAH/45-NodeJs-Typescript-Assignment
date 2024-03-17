43b76e3aaa0c5dac8c0dc092b81c86a6aa122492
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-18 02:10:44 +0500

    Add files via upload

---
+++ 25.Alien Color/color.js
@@ -0,0 +1,8 @@
+"use strict";
+let alienColor = "black";
+if (alienColor === "black") {
+    console.log('alien color is black, player just earned 5 points.');
+}
+if (alienColor === "white") {
+    console.log("alien color is white, 0 point rewarded");
+}
