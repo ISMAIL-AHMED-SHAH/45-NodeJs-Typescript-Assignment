e7702148593561507c11baac61b506c89b059464
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-18 01:58:19 +0500

    Add files via upload

---
+++ 21.Objects/main.js
@@ -0,0 +1,13 @@
+;
+var sportsCar = {
+    modelName: "Ferrari",
+    founder: "Enzo Ferrari",
+    yearOf_launching: "1939"
+};
+console.log(sportsCar);
+// let book: { title: string; author: string; yearPublished: number } = {
+//     title: "The Hobbit",
+//     author: "J.R.R. Tolkien",
+//     yearPublished: 1937
+// };
+// console.log(`Book Info: ${book.title} by ${book.author}, published in ${book.yearPublished}`);
