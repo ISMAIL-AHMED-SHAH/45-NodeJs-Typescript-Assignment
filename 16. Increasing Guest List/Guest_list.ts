ced91a4fc668275617d4b403cdf5fb3a898191e7
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-18 01:38:54 +0500

    Add files via upload

---
+++ 16. Increasing Guest List/Guest_list.ts
@@ -0,0 +1,36 @@
+//Roll # 439714
+
+// creating an array
+let Guest_list :string [] = ['ismail','ahmed','shah'];
+
+let absent_Guest = Guest_list[0]
+
+//one guest is unable to come
+console.log(absent_Guest, "is unable to join us.\n");
+
+//a friend is replaced in the list on beginning through splice function 
+Guest_list.splice(0, 1, "Syed")
+
+// adding on the starting index
+Guest_list.unshift("Ameen Alam");
+
+//adding on the ending index
+Guest_list.push("Asharib Ali");
+
+//getting middle index
+let middleIndex: number = Math.floor(Guest_list.length / 2);
+
+//adding new guest on the middle index
+Guest_list.splice(middleIndex, 0, "Okasha Aijaz")
+
+console.log("I'm glad to announce that we've found a bigger table for our dinner gathering.")
+
+
+Guest_list.forEach(guest => console.log(`Dear ${guest} I am hosting a party on coming weekend.Looking forward to having a fantastic time together.\n`));
+
+
+
+
+
+
+
