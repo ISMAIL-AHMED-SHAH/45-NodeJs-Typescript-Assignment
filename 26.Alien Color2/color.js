f23946606d4facd5c82a81dfad9ed3b903789773
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-18 02:12:57 +0500

    Add files via upload

---
+++ 26.Alien Color2/color.js
@@ -0,0 +1,14 @@
+"use strict";
+let alienColor = 'green';
+if (alienColor === 'green') {
+    console.log('player just earned 5 points on shooting the green alien');
+}
+else {
+    console.log('player just earned 10 points,');
+}
+if (alienColor === 'black') {
+    console.log('player just earned 5 points on shooting the green alien');
+}
+else {
+    console.log('player just earned 10 points.');
+}
---
+++ 26.Alien Color2/color.ts
@@ -0,0 +1,15 @@
+let alienColor = 'green';
+
+if (alienColor === 'green'){
+    console.log('player just earned 5 points on shooting the green alien');
+}
+else{
+    console.log('player just earned 10 points,');
+}
+
+if (alienColor === 'black'){
+    console.log('player just earned 5 points on shooting the green alien');
+}
+else{
+    console.log('player just earned 10 points.');
+}
