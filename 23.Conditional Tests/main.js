46bbb830fa6cf69a5f9b87f7c007a9ba9b13c73a
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-18 02:05:22 +0500

    Add files via upload

---
+++ 23.Conditional Tests/main.js
@@ -0,0 +1,35 @@
+var dollar = 1;
+var rupee = 100;
+//Test 1
+console.log("i predict dollar is equal to 1");
+console.log(dollar == 1);
+//Test 2
+console.log("\ni predict rupee is equal to 100");
+console.log(rupee == 100);
+//Test 3
+console.log("\ni predict dollar is not equal to rupee");
+console.log(dollar != rupee);
+//Test 4
+console.log("\ni predict dollar is less than rupee");
+console.log(dollar < rupee);
+//Test 5
+console.log("\ni predict rupee is greater than dollar");
+console.log(rupee > 1);
+//Test 6
+console.log("\ni predict dollar is equal to rupee");
+console.log(dollar == rupee);
+//Test 7
+console.log("\ni predict dollar is equal to 100");
+console.log(dollar == 100);
+//Test 8
+console.log("\nrupee is equal to 1");
+console.log(rupee == 1);
+//Test 9
+console.log("\n100 is not equal to rupee");
+console.log(100 != rupee);
+//Test 10
+console.log("\nrupee is less than 100 dollar");
+console.log(rupee < 100);
+//Test 11
+console.log("\n200 is less than 100");
+console.log(200 < 100);
