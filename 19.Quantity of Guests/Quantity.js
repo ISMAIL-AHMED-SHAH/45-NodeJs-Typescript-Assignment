0e776c4281076c5cfde64f3e53b2f50b83d71be9
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-18 01:49:32 +0500

    Add files via upload

---
+++ 19.Quantity of Guests/Quantity.js
@@ -0,0 +1,5 @@
+//Roll # 439714
+var Guest_list = ['ismail', 'ahmed', 'shah', 'Syed'];
+// Guest_list.forEach(Guest_list => console.log(`Dear Mr. ${Guest_list} I am hosting a party on coming weekend. Looking forward to having a fantastic time together.\n Warm Regards\n`));
+var lengthGuest = Guest_list.length;
+console.log("I am inviting ".concat(lengthGuest, " people to dinner."));
---
+++ 19.Quantity of Guests/Quantity.ts
@@ -0,0 +1,10 @@
+//Roll # 439714
+
+let Guest_list = ['ismail', 'ahmed', 'shah','Syed'];
+
+// Guest_list.forEach(Guest_list => console.log(`Dear Mr. ${Guest_list} I am hosting a party on coming weekend. Looking forward to having a fantastic time together.\n Warm Regards\n`));
+
+let lengthGuest: number = Guest_list.length;
+
+console.log(`I am inviting ${lengthGuest} people to dinner.`);
+
