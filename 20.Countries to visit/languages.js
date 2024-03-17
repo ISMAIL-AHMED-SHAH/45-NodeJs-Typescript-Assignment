671748d82b36fb7fe1bb73895d285d85313ce85f
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-18 01:55:44 +0500

    Add files via upload

---
+++ 20.Countries to visit/languages.js
@@ -0,0 +1,3 @@
+var languages = ["Typescript", "Javascript", "Python", "Java", "C++"];
+console.log("Languages I'd like to Learn:");
+languages.forEach(function (languages) { return console.log(languages); });
---
+++ 20.Countries to visit/languages.ts
@@ -0,0 +1,5 @@
+let languages: string[] = ["Typescript", "Javascript", "Python", "Java", "C++"];
+
+console.log("Languages I'd like to Learn:");
+
+languages.forEach(languages => console.log(languages));
