f76d1a8e44732ae3d73245b7ef4a86d681e34bd8
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-26 09:03:02 +0500

    45 Assignments

---
+++ 41.Magicians/magicians.js
@@ -0,0 +1,9 @@
+//Solution for Question # 41
+// Array magicians
+var magicians = ["Samri", "Hamoon", "Bil'Batorhi"];
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
---
+++ 41.Magicians/magicians.ts
@@ -0,0 +1,12 @@
+
+//Solution for Question # 41
+// Array magicians
+const magicians: string[] = ["Samri", "Hamoon", "Bil'Batorhi"];
+
+function show_magicians(magicians: string[]){
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+
+show_magicians(magicians);
